ec4f2973b90fbfb8668034b410c5ffe2909c3152
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2025-05-12 10:46:17 +0200

    #update

--- src/components/ThreeDScene/ThreeDScene.tsx
+++ src/components/ThreeDScene/ThreeDScene.tsx
@@ -1,19 +1,22 @@
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
-import Cube from "components/TestCube/TestCube";
-import TestModel from "components/TestModel/TestModel";
+import { OrbitControls, Environment } from "@react-three/drei";
 import ItemModel from "components/ItemModel/ItemModel";
 import style from "./ThreeDScene.module.sass";
 
 const ThreeDScene = () => {
   return (
     <div className={style.container}>
-      <Canvas camera={{ position: [3, 3, 3] }}>
-        <ambientLight intensity={0.5} />
-        <pointLight position={[5, 5, 5]} />
-        {/* <Cube /> */}
-        {/* <TestModel /> */}
-        <ItemModel modelName="bird" scale={0.05} />
+      <Canvas
+        camera={{ position: [3, 3, 3], fov: 50 }}
+        gl={{ antialias: true, toneMappingExposure: 1 }}
+      >
+        <ambientLight intensity={0.3} />
+        <pointLight position={[10, 10, 10]} intensity={0.8} />
+        <directionalLight position={[5, 5, 5]} intensity={0.6} />
+        <Environment preset="sunset" background={false} />
+
+        <ItemModel modelName="bird" scale={10} />
+
         <OrbitControls />
       </Canvas>
     </div>
